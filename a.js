28e798403ba70c9e20200395ca9ca77220bc7fa6
Pan Zhiyong <pan.zhiyong@cybertech.com.cn>
2016-06-27 11:58:07 +0800

    compile, eval, and solve.

--- a.js
+++ a.js
@@ -1,21 +1,11 @@
+/**
+ * 给定操作数和结果，找出算术表达式。如
+ *  5 5 5 1四个数字,用加减乘除,结果等于24
+ */
 'use strict';
 
-let x = 4
-let vals = ['x', 'y', 'z']
-let binOps = ['+', '-', '*', '/']
-let uniOps = ['-', '√']
-
-//vals.forEach(v => {
-//  v.binOp = ''
-//  v.uniOp = ''
-//})
-
-//vals[0].binOp = '+'
-//vals[1].binOp = '-'
-//vals[2].uniOp = '√'
-
-Array.prototype.flatMap = function(lambda) { 
-    return Array.prototype.concat.apply([], this.map(lambda)); 
+Array.prototype.flatMap = function(lambda) {
+    return Array.prototype.concat.apply([], this.map(lambda));
 }
 
 function gen(tokens) {
@@ -31,8 +21,85 @@ function gen(tokens) {
   }
 }
 
+function findExprEnd(expr) {
+  let sep = ' +-*/√()'
+  var unclosed = 0 // count of unclosed parenthesis
+  for (var i = 0; i < expr.length; ++i) {
+    let c = expr[i]
+    if (i > 0 && unclosed == 0 && sep.indexOf(c) != -1)
+      return i
+    if (c == '(')
+      ++unclosed
+    else if (c == ')')
+      --unclosed
+  }
+  return expr.length
+}
+
+function findExprEndTest() {
+  let data = [
+    ['(a+b)*c', 5],
+    ['(a+b) *c', 5],
+    ['(a+b ) *c', 6],
+    ['d/(a+b)*c', 1],
+    ['d2/(a+b)*c', 2],
+    ['(a+(b-c)) * 3', 9],
+    ['(a+(b-c) / 4) * 3', 13],
+    ['(√b-c)) * 3', 6],
+  ]
+
+  console.log('begin test findExprEnd()')
+  data.forEach(i => {
+    let actual = findExprEnd(i[0])
+    console.log(i[0], '=>', i[1])
+    console.assert(actual == i[1], `expect ${i[1]}, got ${actual}`)
+  })
+  console.log('all tests passed')
+}
+
+/** compile math to JavaScript expression. */
+function compile(expr) {
+  if (expr.length < 1) {
+    return expr
+  } else if (expr[0] == '√') {
+    let end = findExprEnd(expr.substr(1))
+    return 'Math.sqrt(' + compile(expr.substr(1, end)) + ')' + compile(expr.substr(1 + end))
+  } else {
+    return expr[0] + compile(expr.substr(1))
+  }
+}
+
+function compileTest() {
+  let data = [
+    ['(a+b)*c', '(a+b)*c'],
+    ['√(a+b) *c', 'Math.sqrt((a+b)) *c'],
+    ['√(a+√b)', 'Math.sqrt((a+Math.sqrt(b)))'],
+    ['√(a+√b-c)', 'Math.sqrt((a+Math.sqrt(b)-c))'],
+    ['√(a+b ) *√c', 'Math.sqrt((a+b )) *Math.sqrt(c)'],
+    ['√d/√(a+√b)*c', 'Math.sqrt(d)/Math.sqrt((a+Math.sqrt(b)))*c'],
+    ['√d2/(a+b)*c', 'Math.sqrt(d2)/(a+b)*c'],
+    ['(a+√(√b-c)) * 3', '(a+Math.sqrt((Math.sqrt(b)-c))) * 3'],
+  ]
+
+  console.log('begin test compile()')
+  data.forEach(i => {
+    let actual = compile(i[0])
+    console.log(i[0], '=>', i[1])
+    console.assert(actual == i[1], `expect ${i[1]}, got ${actual}`)
+  })
+  console.log('all tests passed')
+}
+
+let target = 6
+let x = 5
+let vals = ['x', 'x', 'x', 1]
+let binOps = ['+', '-', '*', '/']
+let uniOps = ['-', '√']
+
 let expr = gen(vals)
-
-//let expr = vals.map(v => v.uniOp + v.x + ' ' + v.binOp + ' ').join('')
-
 console.log(expr.join('\n'))
+
+expr.forEach(e => {
+  if (eval(compile(e)) == target)
+    console.log(e, '=', target)
+})
